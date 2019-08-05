a256b07beb948478416cf2ed70ca2fd2ceae9ee7
Jon-Tait Beason <bugkrusha@Jon-Taits-MacBook-Pro-2.local>
2019-08-05 13:34:09 -0400

    Initial commit

---
+++ Ours/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Ours
+//
+//  Created by Jon-Tait Beason on 8/5/19.
+//  Copyright © 2019 Jon-Tait Beason. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
